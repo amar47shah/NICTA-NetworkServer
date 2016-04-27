dcbbdfeee2c8ed9610f0c6d9cce7cc42d2589484
Amar Shah <amar47shah@gmail.com>
2016-04-26 23:23:13 -0400

    Adjust style

--- src/Network/Server/Chat/Chat.hs
+++ src/Network/Server/Chat/Chat.hs
@@ -3,30 +3,23 @@ module Network.Server.Chat.Chat where
 import Network.Server.Common.Line
 import Network.Server.Chat.Loop
 
+import Control.Arrow ((&&&))
 import Control.Monad.Trans (MonadIO(..))
 import Data.Maybe (fromMaybe)
 import Data.Foldable (msum)
 import Data.IORef (atomicModifyIORef)
 
-type Chat a =
-  IORefLoop Integer a
+type Chat a      = IORefLoop Integer a
+data ChatCommand = Chat String
+                 | Incr
+                 | Unknown String
+                 deriving (Eq, Show)
 
-data ChatCommand =
-  Chat String
-  | Incr
-  | Unknown String
-  deriving (Eq, Show)
+incr :: Chat Integer
+incr = readEnvval >>= liftIO . flip atomicModifyIORef (succ &&& succ)
 
-incr ::
-  Chat Integer
-incr =
-  do e <- readEnvval
-     liftIO $ atomicModifyIORef e (\n -> (n + 1, n + 1))
-
-chat ::
-  IO a
-chat =
-  iorefLoop 0 (readIOEnvval >>= pPutStrLn . show) (process . chatCommand)
+chat :: IO a
+chat = iorefLoop 0 (readIOEnvval >>= pPutStrLn . show) $ process . chatCommand
 
 -- |
 --
@@ -41,14 +34,10 @@ chat =
 --
 -- >>> chatCommand "Nothing"
 -- UNKNOWN "Nothing"
-chatCommand ::
-  String
-  -> ChatCommand
-chatCommand z =
-  Unknown z `fromMaybe` msum [
-                               Chat <$> trimPrefixThen "CHAT" z
-                             , Incr <$ trimPrefixThen "INCR" z
-                             ]
+chatCommand :: String -> ChatCommand
+chatCommand z = Unknown z `fromMaybe` msum [ Chat <$> trimPrefixThen "CHAT" z
+                                           , Incr <$  trimPrefixThen "INCR" z
+                                           ]
 
 process :: ChatCommand -> Chat ()
 process (Chat _)    = error "Chat.process (Chat _)"
